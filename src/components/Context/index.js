b980c9e01958f9eef4d6903a8fab487e991608a1
John Nguyen <nguyen.johnsan@gmail.com>
2020-02-05 07:43:21 -0800

    prep for deploy

--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 import queryString from 'query-string';
-import apiKey from '../../config';
+// import apiKey from '../../config';
 import { animateCSS } from '../../helpers';
 
+const { REACT_APP_API_KEY } = process.env;
 
 const AppContext = React.createContext();
 
@@ -78,7 +79,7 @@ class Provider extends Component {
   /*** Data fetching from flickr API  ***/
   // fetch data from flickr API
   fetchData = async (query) => {
-    const getUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
+    const getUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${REACT_APP_API_KEY}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
     return await axios
       .get(getUrl)
       .then(response => {
@@ -102,7 +103,7 @@ class Provider extends Component {
   // grab data and update state for PhotoContainer
   performSearch = (query = 'cats') => {
     this.setState({ loading: true });
-    const getUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
+    const getUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${REACT_APP_API_KEY}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
     
     this.fetchData(getUrl)
       .then(response => {
